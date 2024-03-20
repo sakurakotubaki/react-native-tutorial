5ba5c9b7434fb815c382935e9c675a74649cf770
Jboy422 <gitonly543@gmail.com>
2024-03-20 20:45:44 +0900

    feat: http get

--- App.tsx
+++ App.tsx
@@ -1,31 +1,10 @@
-import React, { useState } from 'react';
-import { Button, Text, View, StyleSheet } from 'react-native';
+import GetAPI from "./components/GetAPI";
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>You have pushed the button this many times:</Text>
-      <Text style={styles.counter}>{counter}</Text>
-      <Button title="Increment" onPress={() => setCounter(counter + 1)} />
-    </View>
+    <GetAPI />
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 24,
-  },
-  counter: {
-    fontSize: 48,
-    margin: 20,
-  },
-});
 
 export default App;
